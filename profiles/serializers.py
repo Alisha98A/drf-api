cdc4e2ab19a4d7439d26bebbea4f803081ef0233
Alisha <alishaanderssonn@icloud.com>
2025-03-26 19:26:47 +0100

    d

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,11 +3,10 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    owner: serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 
-
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image'
         ]
